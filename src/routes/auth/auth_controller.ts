5bd06531b33deafebba84424b0b2341b165d1567
LaithMahdi <mahdilaith380@gmail.com>
2025-07-03 21:35:32 +0100

    Add user update functionality and schema validation

--- src/routes/auth/auth_controller.ts
+++ src/routes/auth/auth_controller.ts
@@ -1,13 +1,11 @@
 import { zValidator } from "@hono/zod-validator";
 import { Hono } from "hono";
 import argon2 from "argon2";
-import { registerSchema } from "./schema";
+import { registerSchema, updateUserSchema } from "./schema";
 import { db } from "@/lib/prisma_client";
 
-const app = new Hono().post(
-  "/register",
-  zValidator("json", registerSchema),
-  async (c) => {
+const app = new Hono()
+  .post("/register", zValidator("json", registerSchema), async (c) => {
     try {
       const {
         service,
@@ -34,7 +32,6 @@ const app = new Hono().post(
       const hashedPassword = await argon2.hash(password);
 
       //  create the user in the database
-
       const newUser = await db.user.create({
         data: {
           name,
@@ -60,7 +57,80 @@ const app = new Hono().post(
       console.error("Error during registration:", error);
       return c.json({ error: "An error occurred during registration." }, 500);
     }
-  }
-);
+  })
+  .put("/update/:id", zValidator("json", updateUserSchema), async (c) => {
+    try {
+      const {
+        cinNumber,
+        cnssNumber,
+        dateOfBirth,
+        jobTitle,
+        maritalStatus,
+        name,
+        nationality,
+        phone,
+        service,
+      } = c.req.valid("json");
+
+      const { id } = c.req.param();
+
+      // Check if the user exists
+      const user = await db.user.findUnique({
+        where: { id },
+      });
+
+      if (!user) {
+        return c.json({ error: "User not found." }, 404);
+      }
+
+      // Update the user in the database
+      const updatedUser = await db.user.update({
+        where: { id },
+        data: {
+          name,
+          phone,
+          dateOfBirth,
+          jobTitle,
+          maritalStatus,
+          cinNumber,
+          cnssNumber,
+          nationality,
+          service,
+        },
+      });
+
+      return c.json({
+        message: "User updated successfully.",
+        updatedUser,
+      });
+    } catch (error) {
+      console.error("Error during update:", error);
+      return c.json({ error: "An error occurred during update." }, 500);
+    }
+  })
+  .get("/all", async (c) => {
+    try {
+      const users = await db.user.findMany({
+        select: {
+          id: true,
+          name: true,
+          email: true,
+          phone: true,
+          role: true,
+          dateOfBirth: true,
+          cinNumber: true,
+          cnssNumber: true,
+          maritalStatus: true,
+          jobTitle: true,
+          service: true,
+        },
+      });
+
+      return c.json({ message: "Users fetched successfully.", data: users });
+    } catch (error) {
+      console.error("Error fetching users:", error);
+      return c.json({ error: "An error occurred while fetching users." }, 500);
+    }
+  });
 
 export default app;
--- src/routes/auth/schema/index.ts
+++ src/routes/auth/schema/index.ts
@@ -15,3 +15,15 @@ export const registerSchema = z.object({
   jobTitle: z.string().min(1, "Job title is required"),
   service: z.string().min(1, "Service is required"),
 });
+
+export const updateUserSchema = z.object({
+  name: z.string().min(1, "Name is required"),
+  phone: z.string().min(1, "Phone number is required"),
+  dateOfBirth: z.string().min(1, "Date of birth is required"),
+  nationality: z.string().min(1, "Nationality is required"),
+  cinNumber: z.coerce.number().min(1, "CIN number is required"),
+  cnssNumber: z.coerce.number().min(1, "CNSS number is required"),
+  maritalStatus: z.nativeEnum(MaritalStatus).default(MaritalStatus.SINGLE),
+  jobTitle: z.string().min(1, "Job title is required"),
+  service: z.string().min(1, "Service is required"),
+});
