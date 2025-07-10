82b0c194262ff2b70b4b9efa2d85b782e21b997f
LaithMahdi <mahdilaith380@gmail.com>
2025-07-10 20:33:07 +0100

    Add child controller route to the main application

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import { prettyJSON } from "hono/pretty-json";
 import { secureHeaders } from "hono/secure-headers";
 import { timing } from "hono/timing";
 import auth_controller from "@/routes/auth/auth_controller";
+import child_controller from "@/routes/children/children_controller";
 import { cors } from "hono/cors";
 
 const app = new Hono()
@@ -11,7 +12,8 @@ const app = new Hono()
   .use("*", prettyJSON())
   .use("*", secureHeaders())
   .use("*", timing())
-  .route("/auth", auth_controller);
+  .route("/auth", auth_controller)
+  .route("/child", child_controller);
 
 export type AppType = typeof app;
 
